9572419ce73eacc88f1b58da46f8a92e224c6ae7
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-26 16:16:16 -0300

    Cria programmesController

---
+++ src/controllers/programmesController.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const dayjs = require('dayjs');
+
+module.exports = () => {
+  const programmes = {};
+
+  programmes.listProgrammes = async (req, res) => {
+    const { date } = req.body;
+    let dateFormatted = dayjs(date).format('YYYY-MM-DD');
+
+    if (!dayjs(date).isValid()) {
+      dateFormatted = date.split('/').reverse().join('-');
+    }
+
+    const dateRequest = req.body.date ? `?date=${dateFormatted}` : '';
+
+    try {
+      const { data } = await axios.get(`https://epg-api.video.globo.com/programmes/1337${dateRequest}`);
+
+      return res.json({ data: data.programme });
+    } catch (error) {
+      return new Error(error);
+    }
+  };
+
+  return programmes;
+};
