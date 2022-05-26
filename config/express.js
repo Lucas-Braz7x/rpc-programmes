5a9eed15666b346ef1b8f2e13d8a10c51587c9ce
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-26 16:53:27 -0300

    Configura o cors

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('config');
+const cors = require('cors');
 const route = require('../src/routes/route');
 
 module.exports = () => {
@@ -8,6 +9,7 @@ module.exports = () => {
 
   app.set('port', process.env.PORT || config.get('server.port'));
 
+  app.use(cors());
   app.use(bodyParser.json());
 
   (route)(app);
