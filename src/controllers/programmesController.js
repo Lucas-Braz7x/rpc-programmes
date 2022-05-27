bba8d949afdac5156adcf1505116dcba2c6c5919
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-27 03:48:03 -0300

    Cria nova rota

--- src/controllers/programmesController.js
+++ src/controllers/programmesController.js
@@ -6,13 +6,34 @@ module.exports = () => {
 
   programmes.listProgrammes = async (req, res) => {
     const { date } = req.body;
+
     let dateFormatted = dayjs(date).format('YYYY-MM-DD');
 
     if (!dayjs(date).isValid()) {
       dateFormatted = date.split('/').reverse().join('-');
     }
 
-    const dateRequest = req.body.date ? `?date=${dateFormatted}` : '';
+    const dateRequest = date ? `?date=${dateFormatted}` : '';
+
+    try {
+      const { data } = await axios.get(`https://epg-api.video.globo.com/programmes/1337${dateRequest}`);
+
+      return res.json({ data: data.programme });
+    } catch (error) {
+      return new Error(error);
+    }
+  };
+
+  programmes.listProgrammesOnNewDate = async (req, res) => {
+    const { date } = req.params;
+
+    let dateFormatted = dayjs(date).format('YYYY-MM-DD');
+
+    if (!dayjs(date).isValid()) {
+      dateFormatted = date.split('/').reverse().join('-');
+    }
+
+    const dateRequest = date ? `?date=${dateFormatted}` : '';
 
     try {
       const { data } = await axios.get(`https://epg-api.video.globo.com/programmes/1337${dateRequest}`);
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,4 +3,7 @@ const controller = require('../controllers/programmesController')();
 module.exports = (app) => {
   app.route('/programmes')
     .get(controller.listProgrammes);
+
+  app.route('/programmes/:date')
+    .get(controller.listProgrammesOnNewDate);
 };
