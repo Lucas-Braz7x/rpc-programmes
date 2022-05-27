de3dcad946690536cc877d13ea743e3abf5dbe99
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-27 02:36:52 -0300

    Cria Header responsivo

---
+++ src/components/Navigation/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const NavigationContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  padding: 9px 12px;
+  background: #016597;
+
+  > div {
+    font-family: 'Poppins';
+    font-weight: 400;
+    font-size: 32px;
+    color: #ffffff;
+    text-transform: uppercase;
+  }
+`;
